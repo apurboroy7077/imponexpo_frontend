19f231b7332adab69db58176d0a6e7b9e85202f6
Apurbo Roy <apurboroy7077@gmail.com>
2024-04-16 04:30:02 +0600

    version_1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar1 from "@/components/Navbar1";
+import Navbar2 from "@/components/Navbar2";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Navbar1 />
+      <Navbar2 />
     </>
   );
 }

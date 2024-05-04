6ebf8f224f7311a737d97c9d55ec7949784c0308
Apurbo Roy <apurboroy7077@gmail.com>
2024-05-04 21:29:06 +0600

    Added Confirm Order Page.

---
+++ src/app/confirmation/page.tsx
@@ -0,0 +1,22 @@
+import ExclusiveOfferSection from "@/components/ExclusiveOfferSection";
+import Footer3 from "@/components/Footer3";
+import Navbar3 from "@/components/Navbar3";
+import OrderingProcedure from "@/components/basics/OrderingProcedure";
+import ConfirmationPaymentSection from "@/components/payment/ConfirmationPaymentSection";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar3 />
+      <main className="bg-[#e5e5e5]">
+        <OrderingProcedure />
+        <ConfirmationPaymentSection />
+        <ExclusiveOfferSection />
+      </main>
+      <Footer3 />
+    </>
+  );
+};
+
+export default page;
---
+++ src/components/payment/ConfirmationPaymentSection.tsx
@@ -0,0 +1,239 @@
+import React from "react";
+import TheSection2 from "../TheSection2";
+
+const ConfirmationPaymentSection = () => {
+  return (
+    <TheSection2>
+      <div className="lg:w-[90%] lg:m-auto lg:mt-10 lg:mb-10 lg:px-10 lg:py-16 lg:rounded-xl box-shadow-style-1-lg">
+        <div>
+          <div className="text-[#1c1c1c] font-bold lg:text-2xl">
+            Complete Payment
+          </div>
+        </div>
+        <div>
+          <div className="mt-5 text-sm lg:text-lg font-medium text-[#1d5ec9] border-b-[2px] border-[#1d5ec9] pb-1">
+            Wire Transfer
+          </div>
+        </div>
+        <div>
+          <div className="font-medium mt-5 lg:text-lg">Bank Details</div>
+        </div>
+        <div>
+          <div className="mt-16 lg:mt-10">
+            <div>
+              <div className="lg:flex lg:gap-10">
+                <div className="lg:w-[50%]">
+                  <div>
+                    <div className="text-[#696969] text-sm lg:text-base font-medium">
+                      Beneficiary Name
+                    </div>
+                  </div>
+                  <div>
+                    <div className="mt-2">
+                      <input
+                        className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:w-[50%]">
+                  <div className="mt-5 lg:mt-0">
+                    <div>
+                      <div className="text-[#696969] text-sm lg:text-base font-medium">
+                        Description
+                      </div>
+                    </div>
+                    <div>
+                      <div className="mt-2">
+                        <input
+                          className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                          type="text"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    Account Number
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    Beneficiary Address
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="font-medium mt-5 lg:text-lg">
+                Wire Transfer (USD)
+              </div>
+            </div>
+            <div>
+              <div className="lg:flex lg:gap-10 mt-5 lg:mt-10">
+                <div className="lg:w-[50%]">
+                  <div>
+                    <div className="text-[#696969] text-sm lg:text-base font-medium">
+                      Bank Name
+                    </div>
+                  </div>
+                  <div>
+                    <div className="mt-2">
+                      <input
+                        className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:w-[50%]">
+                  <div className="mt-5 lg:mt-0">
+                    <div>
+                      <div className="text-[#696969] text-sm lg:text-base font-medium">
+                        Business Identity Code
+                      </div>
+                    </div>
+                    <div>
+                      <div className="mt-2">
+                        <input
+                          className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                          type="text"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    Account Number
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    ABA/ACH Routing Number fot Wire Transfer within the USA
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    Bank Address
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="font-medium mt-5 lg:text-lg">Tax Information</div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    Address
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="font-medium mt-5 lg:text-lg">Payment Details</div>
+            </div>
+            <div>
+              <div className="mt-5">
+                <div>
+                  <div className="text-[#696969] text-sm lg:text-base font-medium">
+                    Amount
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full outline-none border-b-[2px] text-sm lg:text-base text-[#1c1c1c] font-medium"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="text-center mt-5 lg:mt-10">
+                <button className="bg-[#1d5ec9] text-white font-medium text-sm lg:text-lg px-5 lg:px-16 py-2 lg:py-3 rounded">
+                  Done
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </TheSection2>
+  );
+};
+
+export default ConfirmationPaymentSection;

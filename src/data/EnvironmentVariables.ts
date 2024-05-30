0fe780bd623edca5b0747c414d3f8ebf4583de52
Apurbo Roy <apurboroy7077@gmail.com>
2024-05-30 11:41:02 +0600

    online test.

--- src/data/EnvironmentVariables.ts
+++ src/data/EnvironmentVariables.ts
@@ -1,5 +1,5 @@
-// const serverURL = `https://imponexpo-backend.onrender.com`;
-const serverURL = `http://localhost:5050`;
+const serverURL = `https://imponexpo-backend.onrender.com`;
+// const serverURL = `http://localhost:5050`;
 const KEYNAME_OF_AUTHENTICATION_TOKEN_IN_LOCALSTORAGE =
   "imponexpo_authentication_token";
 const AUTHENTICATE_USER_WITH_TOKEN_SUB_URL = `/authentication/authenticate-user-with-token`;
